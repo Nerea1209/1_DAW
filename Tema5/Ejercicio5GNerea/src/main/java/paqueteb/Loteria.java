d83c01d3ac532b3b5b781d1c98d4739cfb0ebb9f
nerea <nlopsan0104@g.educaand.es>
2023-02-01 10:21:27 +0100

    Modifica loteria, falta solucionar nullpointerexception

--- Tema5/Ejercicio5GNerea/src/main/java/paqueteb/Loteria.java
+++ Tema5/Ejercicio5GNerea/src/main/java/paqueteb/Loteria.java
@@ -5,84 +5,116 @@
 package paqueteb;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.InputMismatchException;
-import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
-
-
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  *
  * @author Nerea
  */
 public class Loteria {
-    
-    private Map<String, Double> loteria = new HashMap<>();
+
+    private SortedMap<String, Double> loteria = new TreeMap<>();
     private static Scanner teclado = new Scanner(System.in);
-    
-   public static void main(String[] args) {
+
+    public static void main(String[] args) {
+
+        Loteria loteria = new Loteria();
+        loteria.guardarEntrada(generarDecimo(), 400_000);
+        loteria.guardarEntrada(generarDecimo(), 125_000);
+        loteria.guardarEntrada(generarDecimo(), 50_000);
+        loteria.guardarEntrada(generarDecimo(), 20_000);
+        loteria.guardarEntrada(generarDecimo(), 20_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
+        loteria.guardarEntrada(generarDecimo(), 6_000);
         
-       Loteria loteria = new Loteria();
-       loteria.guardarEntrada(generarDecimo(), 400_000);
-       loteria.guardarEntrada(generarDecimo(), 125_000);
-       loteria.guardarEntrada(generarDecimo(), 50_000);
-       loteria.guardarEntrada(generarDecimo(), 20_000);
-       loteria.guardarEntrada(generarDecimo(), 6_000);
-       
-       String decimoUsuario = pedirDecimoUsuario();
-       
-//       if (decimoUsuario.equals(loteria.loteria.)) {
-//           
-//       }
-        
+        loteria.mostrarPremios();
+
+        String decimoUsuario = pedirDecimoUsuario();
+        loteria.decirPremio(decimoUsuario);
+
     }
-    
-    public static String pedirDecimoUsuario(){
+
+    public Double decirPremio(String decimoUsuario) {
+        double premio = 0;
+        double doubleDecimoUSuario = Double.parseDouble(decimoUsuario);
+        if (recibirPremio(decimoUsuario) == 0){
+            if (this.loteria.get(String.valueOf(doubleDecimoUSuario + 1)) == 400_000 
+                    || this.loteria.get(String.valueOf(doubleDecimoUSuario - 1)) == 400_000) {
+                premio = 2_000;
+                System.out.println("Su premio es " + premio);
+            } else if (this.loteria.get(String.valueOf(doubleDecimoUSuario + 1)) == 125_000 
+                    || this.loteria.get(String.valueOf(doubleDecimoUSuario - 1)) == 125_000) {
+                premio = 1_250;
+                System.out.println("Su premio es " + premio);
+            } else if (this.loteria.get(String.valueOf(doubleDecimoUSuario + 1)) == 50_000 
+                    || this.loteria.get(String.valueOf(doubleDecimoUSuario - 1)) == 50_000) {
+                premio = 960;
+                System.out.println("Su premio es " + premio);
+            } else {
+                System.out.println("No tiene ningún premio.");
+                premio = 0;
+            }
+        }
+        return premio;
+    }
+
+    public static String pedirDecimoUsuario() {
         String decimo = "";
-        do {            
+        do {
             System.out.println("Introduce su número de décimo para comprobar premio");
             try {
-              decimo = teclado.nextLine();
+                decimo = teclado.nextLine();
             } catch (InputMismatchException ime) {
                 teclado.nextLine();
                 System.out.println("""
                                    No has introducido un número entero.
                                    Por favor, vuelve a intentarlo.""");
             }
-        } while (Integer.parseInt(decimo) < 0 || Integer.parseInt(decimo) > 99999);
+        } while (Double.parseDouble(decimo) < 0 || Double.parseDouble(decimo) > 99999);
         return decimo;
     }
 
-    public void guardarEntrada(String decimo, double premio){
+    public void guardarEntrada(String decimo, double premio) {
         loteria.put(decimo, premio);
     }
-    
-    public double decirPremio(String decimo){
-        return loteria.get(decimo);
-    }
-    
-    public Set<String> crearListaDecimosPremiados(){
+
+    public Set<String> crearListaDecimosPremiados() {
         return loteria.keySet();
     }
-    
-    public Collection<Double> crearListaPremios(){
+
+    public Collection<Double> crearListaPremios() {
         return loteria.values();
     }
-    
-    public void mostrarLoteria(){
-         loteria.forEach((clave, valor) -> System.out.println( clave + "  ------  " + valor));
+
+    public void mostrarLoteria() {
+        loteria.forEach((clave, valor) -> System.out.println(clave + "  ------  " + valor));
     }
-    
-    public static String generarDecimo(){
+
+    public static String generarDecimo() {
         return "" + new Random().nextInt(0, 100000);
     }
     
- 
-   
+    public void mostrarPremios(){
+         loteria.forEach((clave, valor) -> System.out.println( clave + "  ------  " + valor));
+    }
     
-    
-    
+    public double recibirPremio(String decimo){
+        if (this.loteria.get(decimo) != null){
+        return this.loteria.get(decimo);
+        } else {
+            return 0;
+        }
+    }
+
 }
