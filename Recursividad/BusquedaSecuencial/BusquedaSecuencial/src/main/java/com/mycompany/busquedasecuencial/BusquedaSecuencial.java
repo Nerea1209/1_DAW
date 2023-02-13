cb9e8a519eb3676dc33bd75fd3f0c1611817f01a
nerea <nlopsan0104@g.educaand.es>
2023-02-13 14:34:12 +0100

    Termina busqueda secuencial

--- Recursividad/BusquedaSecuencial/BusquedaSecuencial/src/main/java/com/mycompany/busquedasecuencial/BusquedaSecuencial.java
+++ Recursividad/BusquedaSecuencial/BusquedaSecuencial/src/main/java/com/mycompany/busquedasecuencial/BusquedaSecuencial.java
@@ -10,8 +10,34 @@ package com.mycompany.busquedasecuencial;
  * @author nerea
  */
 public class BusquedaSecuencial {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] array = {1, 2, 3, 100, 5, 9};
+        System.out.println(busquedaSecuencial(array, 100, 0));
+        System.out.println(busquedaSecuencialRecursiva(array, 100, 0));
+        
     }
+
+    public static int busquedaSecuencial(int[] arrayEnteros, int numeroABuscar, int posicion){
+        int pos = -1;
+        for (int i = 0; i < arrayEnteros.length; i++) {
+            if (arrayEnteros[i] == numeroABuscar) {
+                pos = i;
+            }
+        }
+        return pos;
+    }
+    
+    public static int busquedaSecuencialRecursiva(int[] arrayEnteros, int numeroABuscar, int posicion){
+        int pos = -1;
+        if (posicion >= 0 && posicion < arrayEnteros.length) {
+            if (arrayEnteros[posicion] == numeroABuscar) {
+                pos = posicion;
+            } else {
+                pos = busquedaSecuencialRecursiva(arrayEnteros, numeroABuscar, posicion + 1);
+            }
+        }
+        return pos;
+    }
+    
 }
