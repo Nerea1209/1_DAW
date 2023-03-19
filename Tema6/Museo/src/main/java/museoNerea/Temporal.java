36139fe6d8c870e266e799871dd58425990cf7a7
Nerea1209 <nlopsan0104@g.educaand.es>
2023-03-19 19:29:46 +0100

    Crea la clase temporal

---
+++ Tema6/Museo/src/main/java/museoNerea/Temporal.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package museoNerea;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Nerea
+ */
+public final class Temporal extends Sala {
+    
+    private LocalDate fechaIni, fechaFin;
+
+    public Temporal() {
+    }
+
+    public Temporal(LocalDate fechaIni, LocalDate fechaFin, int salaID) {
+        super(salaID);
+        this.fechaIni = fechaIni;
+        this.fechaFin = fechaFin;
+    }
+
+    public LocalDate getFechaIni() {
+        return fechaIni;
+    }
+
+    public void setFechaIni(LocalDate fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString()).append("Temporal{");
+        sb.append("fechaIni=").append(fechaIni);
+        sb.append(", fechaFin=").append(fechaFin);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+}
