5146aa8b8b539b668e980f27801bccb0bb7a0256
Nerea1209 <nlopsan0104@g.educaand.es>
2023-04-10 21:25:00 +0200

    Modifica cursores y soluciona si el primero es negativo

--- InterfazGrafica/Calculadora/src/main/java/calculadoraNerea/PanelCalculadora.java
+++ InterfazGrafica/Calculadora/src/main/java/calculadoraNerea/PanelCalculadora.java
@@ -16,14 +16,13 @@ import java.util.regex.Pattern;
 public class PanelCalculadora extends javax.swing.JPanel {
 
     final String regex = "[*+/[-]]";
-    String resultadoFinal="";
+    String resultadoFinal = "";
 
     /**
      * Creates new form PanelCalculadora
      */
     public PanelCalculadora() {
         initComponents();
-
     }
 
     /**
@@ -68,7 +67,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jTextArea1.setRows(3);
         jTextArea1.setAlignmentX(1.0F);
         jTextArea1.setAlignmentY(1.0F);
-        jTextArea1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jTextArea1.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
         jTextArea1.setMargin(new java.awt.Insets(4, 4, 4, 4));
         jScrollPane1.setViewportView(jTextArea1);
 
@@ -77,6 +76,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton1.setForeground(new java.awt.Color(51, 153, 255));
         jButton1.setText("8");
         jButton1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton1.setFocusable(false);
         jButton1.setName("8"); // NOI18N
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -90,6 +90,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton2.setForeground(new java.awt.Color(51, 153, 255));
         jButton2.setText("7");
         jButton2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton2.setFocusable(false);
         jButton2.setName("7"); // NOI18N
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -103,6 +104,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton4.setForeground(new java.awt.Color(51, 153, 255));
         jButton4.setText("9");
         jButton4.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton4.setFocusable(false);
         jButton4.setName("9"); // NOI18N
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -116,6 +118,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton6.setForeground(new java.awt.Color(51, 153, 255));
         jButton6.setText("4");
         jButton6.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton6.setFocusable(false);
         jButton6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -128,6 +131,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton8.setForeground(new java.awt.Color(51, 153, 255));
         jButton8.setText("5");
         jButton8.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton8.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton8.setFocusable(false);
         jButton8.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -140,6 +144,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton9.setForeground(new java.awt.Color(51, 153, 255));
         jButton9.setText("6");
         jButton9.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton9.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton9.setFocusable(false);
         jButton9.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -152,6 +157,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton11.setForeground(new java.awt.Color(51, 153, 255));
         jButton11.setText("1");
         jButton11.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton11.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton11.setFocusable(false);
         jButton11.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -164,6 +170,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton12.setForeground(new java.awt.Color(51, 153, 255));
         jButton12.setText("2");
         jButton12.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton12.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton12.setFocusable(false);
         jButton12.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -176,6 +183,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton13.setForeground(new java.awt.Color(51, 153, 255));
         jButton13.setText("3");
         jButton13.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton13.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton13.setFocusable(false);
         jButton13.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -188,6 +196,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton15.setForeground(new java.awt.Color(255, 255, 255));
         jButton15.setText("/");
         jButton15.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 102, 102), 3, true));
+        jButton15.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton15.setFocusable(false);
         jButton15.setName("/"); // NOI18N
         jButton15.addActionListener(new java.awt.event.ActionListener() {
@@ -201,6 +210,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton17.setForeground(new java.awt.Color(255, 255, 255));
         jButton17.setText("*");
         jButton17.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 102, 102), 3, true));
+        jButton17.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton17.setFocusable(false);
         jButton17.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -213,6 +223,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton18.setForeground(new java.awt.Color(255, 255, 255));
         jButton18.setText("+");
         jButton18.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 102, 102), 3, true));
+        jButton18.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton18.setFocusable(false);
         jButton18.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -225,6 +236,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton19.setForeground(new java.awt.Color(255, 255, 255));
         jButton19.setText("-");
         jButton19.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 102, 102), 3, true));
+        jButton19.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton19.setFocusable(false);
         jButton19.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -237,6 +249,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton20.setForeground(new java.awt.Color(255, 255, 255));
         jButton20.setText("=");
         jButton20.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 153, 255), 2, true));
+        jButton20.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton20.setFocusable(false);
         jButton20.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -249,6 +262,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton14.setForeground(new java.awt.Color(51, 153, 255));
         jButton14.setText("0");
         jButton14.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton14.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton14.setFocusable(false);
         jButton14.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -261,6 +275,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton16.setForeground(new java.awt.Color(51, 153, 255));
         jButton16.setText("00");
         jButton16.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton16.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton16.setFocusable(false);
         jButton16.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -273,6 +288,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton21.setForeground(new java.awt.Color(51, 153, 255));
         jButton21.setText(".");
         jButton21.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 2, true));
+        jButton21.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton21.setFocusable(false);
         jButton21.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -285,6 +301,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton5.setForeground(new java.awt.Color(255, 255, 255));
         jButton5.setText("AC");
         jButton5.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 0), 2, true));
+        jButton5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton5.setFocusable(false);
         jButton5.setName("9"); // NOI18N
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -298,6 +315,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         jButton10.setForeground(new java.awt.Color(255, 255, 255));
         jButton10.setText("C");
         jButton10.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 0), 2, true));
+        jButton10.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton10.setFocusable(false);
         jButton10.setName("9"); // NOI18N
         jButton10.addActionListener(new java.awt.event.ActionListener() {
@@ -433,11 +451,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton13ActionPerformed
 
     private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton15ActionPerformed
-//        if (!jTextArea1.getText().endsWith(regex)) {
-//            jTextArea1.setText(jTextArea1.getText() + "/");
-//        }
         controlOperaciones("/");
-
     }//GEN-LAST:event_jButton15ActionPerformed
 
     private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
@@ -453,36 +467,37 @@ public class PanelCalculadora extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton19ActionPerformed
 
     private void jButton20ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton20ActionPerformed
-        if (!jTextArea1.getText().endsWith("+") && !jTextArea1.getText().endsWith("/")
-                && !jTextArea1.getText().endsWith("-") && !jTextArea1.getText().endsWith("*")
-                && !jTextArea1.getText().endsWith(".")) {
-        final String string = jTextArea1.getText();
-
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(string);
-
-        String[] partes = string.split(regex);
-        double resultado = Double.parseDouble(partes[0]);
-        int j = 1;
-        while (matcher.find()) {
-
-            for (int i = 0; i <= matcher.groupCount(); i++) {
-
-                resultado = switch (matcher.group(i)) {
-                    case "/" -> resultado / Double.parseDouble(partes[j]);
-                    case "*" ->resultado * Double.parseDouble(partes[j]);
-                    case "+" ->resultado + Double.parseDouble(partes[j]);
-                    default -> resultado - Double.parseDouble(partes[j]);
-                };
-                j++;
+        if (ultimoEsOperando() && !jTextArea1.getText().equals("")) {
+            String string = "";
+            if (primeroEsNegativo()) {
+                string = "0" + jTextArea1.getText();
+            } else {
+                string = jTextArea1.getText();
             }
-        }
-        
-        BigDecimal decimales = new BigDecimal(resultado);
-        // stripTrailingZeros controla que el decimal tenga algún decimal distinto de cero
-        // toPlainString lo pasa a String
-        resultadoFinal = decimales.setScale(2, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
-        jTextArea1.setText(resultadoFinal);
+            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+            final Matcher matcher = pattern.matcher(string);
+
+            String[] partes = string.split(regex);
+            double resultado = Double.parseDouble(partes[0]);
+            int j = 1;
+            while (matcher.find()) {
+
+                for (int i = 0; i <= matcher.groupCount(); i++) {
+                    resultado = switch (matcher.group(i)) {
+                        case "/" ->resultado / Double.parseDouble(partes[j]);
+                        case "*" -> resultado * Double.parseDouble(partes[j]);
+                        case "+" -> resultado + Double.parseDouble(partes[j]);
+                        default -> resultado - Double.parseDouble(partes[j]);
+                    };
+                    j++;
+                }
+            }
+
+            BigDecimal decimales = new BigDecimal(resultado);
+            // stripTrailingZeros controla que el decimal tenga algún decimal distinto de cero
+            // toPlainString lo pasa a String
+            resultadoFinal = decimales.setScale(2, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
+            jTextArea1.setText(resultadoFinal);
         }
     }//GEN-LAST:event_jButton20ActionPerformed
 
@@ -499,7 +514,7 @@ public class PanelCalculadora extends javax.swing.JPanel {
         if (!partes[partes.length - 1].contains(".")) {
             controlOperaciones(".");
         }
-        
+
     }//GEN-LAST:event_jButton21ActionPerformed
 
     // Si le da a AC (all clear) borra todos los numeros insertados
@@ -540,23 +555,28 @@ public class PanelCalculadora extends javax.swing.JPanel {
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 
-    private void controlOperaciones(String valor){
-        if (!jTextArea1.getText().endsWith("+") && !jTextArea1.getText().endsWith("/")
-                && !jTextArea1.getText().endsWith("-") && !jTextArea1.getText().endsWith("*")
-                && !jTextArea1.getText().endsWith(".")) {
+    private void controlOperaciones(String valor) {
+        if (!jTextArea1.getText().equals("") && ultimoEsOperando()) {
             jTextArea1.setText(jTextArea1.getText() + valor);
         }
     }
-    
-    private void imprimirNumero(String numero, java.awt.event.ActionEvent evt){
-        if (!resultadoFinal.equals("") && !jTextArea1.getText().endsWith("+") && !jTextArea1.getText().endsWith("/")
-                && !jTextArea1.getText().endsWith("-") && !jTextArea1.getText().endsWith("*")
-                && !jTextArea1.getText().endsWith(".")) {
+
+    private void imprimirNumero(String numero, java.awt.event.ActionEvent evt) {
+        if (!resultadoFinal.equals("") && ultimoEsOperando()) {
             jButton5ActionPerformed(evt);
         }
         jTextArea1.setText(jTextArea1.getText() + numero);
-        resultadoFinal="";
+        resultadoFinal = "";
     }
+
+    private boolean ultimoEsOperando() {
+        return !jTextArea1.getText().endsWith("+") && !jTextArea1.getText().endsWith("/")
+                && !jTextArea1.getText().endsWith("-") && !jTextArea1.getText().endsWith("*")
+                && !jTextArea1.getText().endsWith(".");
+    }
+    
+    private boolean primeroEsNegativo() {
+        return jTextArea1.getText().startsWith("-");
+    }
+    
 }
-
-
