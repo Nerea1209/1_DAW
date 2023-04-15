6553561df2bf42ed46417ed86c45a2085db7cc9c
Eduardo2044 <eduardoorellanapomares@gmail.com>
2023-04-15 20:57:44 +0200

    Termina parteA y empieza parteB

---
+++ Tema7/Ej7bNerea/src/main/java/ej7bnerea/ParteB.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej7bnerea;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author eduar
+ */
+public class ParteB {
+
+    public static void main(String[] args) {
+        List<Vehiculo> vehiculosLeidos = leerFichero("vehículos.txt");
+
+    }
+
+    public static List<String> leerFichero(String ruta) {
+
+        // Variables para guardar los datos que se van leyendo
+        String[] tokens;
+        String linea;
+
+        System.out.println("Leyendo el fichero: " + ruta);
+
+        // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
+        // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
+        // las operaciones con el archivo
+        try ( Scanner datosFichero = new Scanner(new File(ruta), "UTF-8")) {
+            // hasNextLine devuelve true mientras haya líneas por leer
+            while (datosFichero.hasNextLine()) {
+                // Guarda la línea completa en un String
+                linea = datosFichero.nextLine();
+                // Se guarda en el array de String cada elemento de la
+                // línea en función del carácter separador de campos del fichero CSV
+                tokens = linea.split(":");
+                for (String string : tokens) {
+                    System.out.print(string + "\t");
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- Tema7/Ej7bNerea/src/main/java/ej7bnerea/parteA.java
+++ Tema7/Ej7bNerea/src/main/java/ej7bnerea/parteA.java
@@ -4,6 +4,11 @@
  */
 package ej7bnerea;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -17,6 +22,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class parteA {
 
     private static Random random = new Random();
+    
+    public static void main(String[] args) {
+        List<Vehiculo> vehiculos = crearListaVehiculo(10, 10, 10);
+        List<String> texto = pasarListaString(vehiculos);
+        escribirListaString(texto, "vehículos.txt");
+        
+    }
 
     public static List<Vehiculo> crearListaVehiculo(int turismos, int deportivos, int furgonetas) {
         List<Vehiculo> lista = new ArrayList<>();
@@ -43,5 +55,25 @@ public class parteA {
         }
         return lista;
     }
+    
+    public static List<String> pasarListaString(List<Vehiculo> vehiculos){
+        List<String> lista = new ArrayList<>();
+        for (Vehiculo v : vehiculos) {
+            lista.add(v.toString());
+        }
+        return lista;
+    }
+    
+    // Método para escribir una lista de String, sobreescribiendo si el fichero existe
+    public static void escribirListaString(List<String> lista, String ruta) {
+        
+        try {
+            Files.write(Paths.get(ruta), lista, StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException ex) {
+            System.out.println("Error creando el fichero");
+        }
+    }
+    
 
 }
