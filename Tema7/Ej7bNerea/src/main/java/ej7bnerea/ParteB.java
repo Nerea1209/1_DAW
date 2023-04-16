f372ecba1823021291018f125442da85b350986b
Nerea1209 <nlopsan0104@g.educaand.es>
2023-04-16 21:31:56 +0200

    Termina parte B

--- Tema7/Ej7bNerea/src/main/java/ej7bnerea/ParteB.java
+++ Tema7/Ej7bNerea/src/main/java/ej7bnerea/ParteB.java
@@ -11,8 +11,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -21,37 +24,125 @@ import java.util.Scanner;
 public class ParteB {
 
     public static void main(String[] args) {
-        List<Vehiculo> vehiculosLeidos = leerFichero("vehículos.txt");
-
+        List<Vehiculo> vehiculosLeidos = leerFichero("vehículos.txt", "[0-2] - ([^:\\\\s]+(:[^:\\s]+)+)");
+        System.out.println("Lista de vehículos leídos sin ordenar");
+        vehiculosLeidos.forEach(System.out::println);
+        System.out.println("\nLista de vehículos ordenados por marca");
+        Collections.sort(vehiculosLeidos, (o1, o2) -> o1.getMarca().compareTo(o2.getMarca()));
+        vehiculosLeidos.forEach(System.out::println);
     }
 
-    public static List<String> leerFichero(String ruta) {
+    public static List<Vehiculo> leerFichero(String ruta, String regex) {
 
-        // Variables para guardar los datos que se van leyendo
-        String[] tokens;
-        String linea;
-
-        System.out.println("Leyendo el fichero: " + ruta);
-
-        // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
-        // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
-        // las operaciones con el archivo
-        try ( Scanner datosFichero = new Scanner(new File(ruta), "UTF-8")) {
-            // hasNextLine devuelve true mientras haya líneas por leer
-            while (datosFichero.hasNextLine()) {
-                // Guarda la línea completa en un String
-                linea = datosFichero.nextLine();
-                // Se guarda en el array de String cada elemento de la
-                // línea en función del carácter separador de campos del fichero CSV
-                tokens = linea.split(":");
-                for (String string : tokens) {
-                    System.out.print(string + "\t");
-                }
-                System.out.println();
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        List<String> lineasTextoVehiculos = new ArrayList<>();
+        try {
+            lineasTextoVehiculos = Files.readAllLines(Paths.get(ruta),
+                    StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            System.out.println("Error leyendo el fichero");
         }
+        for (String linea : lineasTextoVehiculos) {
+            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+            final Matcher matcher = pattern.matcher(linea);
+
+            while (matcher.find()) {
+
+                String partes[] = matcher.group(1).split(":");
+                String bastidor = partes[0];
+                String matricula = partes[1];
+                String marca = partes[2];
+                String modelo = partes[3];
+                String color = partes[4];
+                String tarifa = partes[5];
+                String disponible = partes[6];
+                if (linea.startsWith("0"))  {
+                    String numPuerta = partes[7];
+                    vehiculos.add(new Turismo(Integer.parseInt(numPuerta), 
+                            Long.valueOf(bastidor), matricula, obtenerMarca(marca), 
+                            obtenerModelo(modelo), obtenerColor(color), 
+                            Double.parseDouble(tarifa), Boolean.parseBoolean(disponible)));
+                } else if (linea.startsWith("1"))  {
+                    String cc = partes[7];
+                    vehiculos.add(new Deportivo(Integer.parseInt(cc), 
+                            Long.valueOf(bastidor), matricula, obtenerMarca(marca), 
+                            obtenerModelo(modelo), obtenerColor(color), 
+                            Double.parseDouble(tarifa), Boolean.parseBoolean(disponible)));
+                } else if (linea.startsWith("2"))  {
+                    String carga = partes[7];
+                    String volumen = partes[8];
+                    vehiculos.add(new Furgoneta(Integer.parseInt(carga), Integer.parseInt(volumen), 
+                            Long.valueOf(bastidor), matricula, obtenerMarca(marca), 
+                            obtenerModelo(modelo), obtenerColor(color), 
+                            Double.parseDouble(tarifa), Boolean.parseBoolean(disponible)));
+                }
+
+            }
+        }
+
+        return vehiculos;
+
+    }
+    
+    public static Marca obtenerMarca(String marca){
+        return switch (marca) {
+            case "SEAT" -> Marca.SEAT;
+            case "CITROËN" -> Marca.CITROËN;
+            case "FORD" -> Marca.FORD;
+            case "MERCEDES" -> Marca.MERCEDES;
+            case "OPEL" -> Marca.OPEL;
+            case "PEUGEOT" ->  Marca.PEUGEOT;
+            case "RENAULT" -> Marca.RENAULT;
+            case "VOLKSWAGEN" -> Marca.VOLKSWAGEN;
+            default -> { throw new IllegalArgumentException();
+            }
+        };
+    }
+    
+    public static Modelo obtenerModelo(String modelo){
+        return switch (modelo) {
+            case "MÉGANE" -> Modelo.MÉGANE;
+            case "CLIO" -> Modelo.CLIO;
+            case "TWINGO" -> Modelo.TWINGO;
+            case "ARKANA" -> Modelo.ARKANA;
+            case "C3" -> Modelo.C3;
+            case "C4" ->  Modelo.C4;
+            case "DS4" -> Modelo.DS4;
+            case "IBIZA" -> Modelo.IBIZA;
+            case "ATECA" -> Modelo.ATECA;
+            case "LEÓN" -> Modelo.LEÓN;
+            case "E208" -> Modelo.E208;
+            case "ERIFTER" -> Modelo.ERIFTER;
+            case "HIBRID508" -> Modelo.HIBRID508;
+            case "MONDEO" -> Modelo.MONDEO;
+            case "FOCUS" -> Modelo.FOCUS;
+            case "MUNSTANG" -> Modelo.MUNSTANG;
+            case "ASTRA" -> Modelo.ASTRA;
+            case "CORSA" -> Modelo.CORSA;
+            case "GOLF" -> Modelo.GOLF;
+            case "POLO" -> Modelo.POLO;
+            case "PASSAT" -> Modelo.PASSAT;
+            case "GLA" -> Modelo.GLA;
+            case "CLASE_C" -> Modelo.CLASE_C;
+            case "EQE" -> Modelo.EQE;
+            default -> { throw new IllegalArgumentException();
+            }
+        };
+    }
+    
+    public static Color obtenerColor(String color){
+        return switch (color) {
+            case "AZUL" -> Color.AZUL;
+            case "VIOLETA" -> Color.VIOLETA;
+            case "AMARILLO" -> Color.AMARILLO;
+            case "NARANJA" -> Color.NARANJA;
+            case "ROJO" -> Color.ROJO;
+            case "VERDE" -> Color.VERDE;
+            case "BLANCO" -> Color.BLANCO;
+            case "NEGRO" -> Color.NEGRO;
+            default -> { throw new IllegalArgumentException();
+            }
+        };
     }
 
 }
